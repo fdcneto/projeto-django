992ce414a11384ea0fd913cd15f310d935025992
Francisco Das Chagas Neto <art0027632@aluno.firjansenaisesi.com.br>
2025-11-24 21:18:26 -0300

    tudo

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     # O parâmetro "name" é usado para fornecer um identificador exclusivo para esse padrão de URL,
     # que pode ser usado para pesquisas reversas de URL.
     path('', views.home, name='home'),
+	path('usuarios/', views.listar_usuarios_com_filtros, 
+		name='listar_usuarios_com_filtros'),
 	path('usuarios/', views.listar_usuarios, 
 	  name='listar_usuarios'),
-	# path('usuarios/<int:id>/', views.buscar_usuario, name='buscar_usuario'),
-	# path('usuarios/', views.listar_usuarios_com_filtros, name='listar_usuarios_com_filtros'),
+	path('usuarios/<int:id>/', views.buscar_usuario, 
+	  name='buscar_usuario'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -32,5 +32,75 @@ def listar_usuarios(request):
 		'total': len(dados)
 	})
 
+# listar usuarios usando filtros
+def listar_usuarios_com_filtros(request):
 
+	# 1 passo - pegar todos os usuarios
+	usuarios = Usuario.objects.all()
+
+	# 2 passo - filtrar por email
+	# 2.1 - pegar o email da URL
+	email_consulta = request.GET.get('email')
+
+	if email_consulta:
+		usuarios = usuarios.filter(
+			email__icontains=email_consulta
+		)
+	
+	nome_consulta = request.GET.get('nome')
+	if nome_consulta:
+		usuarios = usuarios.filter(
+			nome__icontains=nome_consulta
+		)
+
+	ordem = request.GET.get('ordem', '-criado')
+	# segunda variavel -> formato padrão
+	usuarios = usuarios.order_by(ordem)
+
+	dados = [
+		{
+			'id':usuario.id,
+			'nome': usuario.nome,
+			'email': usuario.email,
+		}
+		for usuario in usuarios
+	]
+	return JsonResponse({
+		'dados':dados,
+		'total': len(dados),
+		'filtros':{
+			'nome': nome_consulta,
+			'email': email_consulta,
+			'ordem': ordem
+		}
+	})
+def buscar_usuario(request, id):
+    """
+    GET /usuarios/1/
+    Busca um usuário específico por ID
+    
+    No Flask:
+    @app.route('/usuarios/<int:id>')
+	def buscar_usuario(id):
+    	usuario = Usuario.query.get(id)
+    """
+    
+    try:
+        usuario = Usuario.objects.get(id=id)
+        
+        dados = {
+            'id': usuario.id,
+            'nome': usuario.nome,
+            'email': usuario.email,
+            'criado': usuario.criado.isoformat(),
+            'atualizado': usuario.atualizado.isoformat()
+        }
+        
+        return JsonResponse(dados)
+        
+    except Usuario.DoesNotExist:
+        return JsonResponse(
+            {'erro': 'Usuário não encontrado'},
+            status=404
+        )
 
